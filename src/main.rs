471052cefadd5d3509ada54b198f78d26217944a
Daniel Allen <105088697+Daniel-S-Allen@users.noreply.github.com>
2026-05-20 18:21:31 -0400

    Implemented OCR detection

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use anyhow::Error;
 use ashpd::desktop::{
     Session, SessionPortal,
     global_shortcuts::{Activated, GlobalShortcuts, NewShortcut},
@@ -5,12 +6,14 @@ use ashpd::desktop::{
 use clap::{Parser, ValueEnum};
 use ftail::Ftail;
 use futures_util::{StreamExt, pin_mut};
+use image::{GrayImage, ImageReader};
+use imageproc::contrast::{ThresholdType, otsu_level, threshold_mut};
 use log::{LevelFilter, debug, error, info, trace};
 use std::{
     fs::{self, File, OpenOptions},
     path::Path,
 };
-
+use tesseract::Tesseract;
 #[derive(Default, Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
@@ -110,15 +113,19 @@ async fn setup_keybinds() -> Result<
 }
 
 async fn key_event_loop(
-    proxy: GlobalShortcuts,
-    session: Session<GlobalShortcuts>,
+    _proxy: GlobalShortcuts,
+    _session: Session<GlobalShortcuts>,
     stream: impl futures_util::Stream<Item = Activated>,
     window_name: &str,
 ) -> ashpd::Result<()> {
     pin_mut!(stream);
+    let mut image_number: u16 = 0;
     while let Some(signal) = stream.next().await {
         match signal.shortcut_id() {
-            "manual_capture" => capture_pressed(window_name),
+            "manual_capture" => {
+                image_number += 1;
+                capture_pressed(window_name, image_number)
+            },
             _ => println!("Unknown"),
         }
     }
@@ -126,34 +133,45 @@ async fn key_event_loop(
     Ok(())
 }
 
-fn capture_pressed(window_name: &str) {
+fn capture_pressed(window_name: &str, image_number: u16) {
     debug!("Capture pressed");
     let window = find_window_by_name(window_name);
     if window.is_some() {
-        take_screenshot(&window.unwrap());
+        take_screenshot(&window.unwrap(), image_number);
+        let path_str = format!("target/windows/window-{}-original.png", &image_number);
+        let text = ocr(Path::new(&path_str), image_number);
+        match text{
+            Ok(text) => debug!("Extracted text: \"{}\"", text),
+            Err(err) => debug!("Could not extract text: {:?}", err)
+        }
+        
     }
 }
 use fs_extra::dir;
 use xcap::Window;
-
 fn find_window_by_name(name: &str) -> Option<Window> {
     let windows = Window::all().unwrap().to_owned();
     let windows_string = windows
         .iter()
-        .map(|w| {
-            match w.title(){
-                Ok(val) => format!("\"{}\"", val),
-                _ => String::from("Unknown")
-            }
-    })
+        .map(|w| match w.title() {
+            Ok(val) => format!("\"{}\"", val),
+            _ => String::from("Unknown"),
+        })
         .reduce(|mut string, title| {
             string.push_str(&format!(", {}", &title));
             return string;
         });
-    debug!("Found windows: {}", windows_string.unwrap_or("No windows found".to_string()));
-    let window = windows
-        .into_iter()
-        .find(|w| w.title().as_deref().unwrap_or_default().trim().eq_ignore_ascii_case(name));
+    debug!(
+        "Found windows: {}",
+        windows_string.unwrap_or("No windows found".to_string())
+    );
+    let window = windows.into_iter().find(|w| {
+        w.title()
+            .as_deref()
+            .unwrap_or_default()
+            .trim()
+            .eq_ignore_ascii_case(name)
+    });
     if window.is_some() {
         debug!("Found window titled {}", name);
     } else {
@@ -162,16 +180,38 @@ fn find_window_by_name(name: &str) -> Option<Window> {
     return window;
 }
 
-fn take_screenshot(window: &Window) {
-    dir::create_all("target/windows", true).unwrap();
+fn take_screenshot(window: &Window, image_number: u16) {
+    dir::create_all("target/windows", false).unwrap();
     if !window.is_minimized().unwrap() {
         let image = window.capture_image().unwrap();
         image
             .save(format!(
-                "target/windows/window-{}.png",
-                &window.title().unwrap().trim()
+                "target/windows/capture-{}-original.png",
+                image_number
             ))
             .unwrap();
         debug!("Took screenshot");
     }
+}
+fn preprocess(image_path: &Path) -> Result<GrayImage, Error> {
+    let image = ImageReader::open(image_path)?.decode()?;
+    let mut grayscale = image.to_luma8();
+    let threshold = otsu_level(&grayscale);
+    threshold_mut(&mut grayscale, threshold, ThresholdType::Binary);
+    return Ok(grayscale);
+}
+fn ocr(image_path: &Path, image_number: u16) -> Result<String, Error> {
+    let mut tesseract = Tesseract::new(None, Some("eng"))?;
+    let grayscale = preprocess(image_path)?;
+    // let temp_path = std::env::temp_dir().join("warframe_util_processed.png");
+    let temp_path = format!("target/windows/capture-{}-processed.png", image_number);
+    let _ = grayscale.save(&temp_path);
+    tesseract = tesseract.set_image(&temp_path.to_string())?;
+    tesseract = tesseract.set_variable(
+        "tessedit_char_whitelist",
+        // "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz ",
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz 123456789,+",
+    )?;
+    let text = tesseract.get_text()?;
+    return Ok(text);
 }
